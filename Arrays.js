6a294b1ac58e3aa5ab6a3749ed48458d41e126ab
ChineseChikki <112833476+ChineseChikki@users.noreply.github.com>
2022-09-11 17:00:30 +0100

    Last commit

--- Arrays.js
+++ Arrays.js
@@ -33,12 +33,11 @@ while (rep <= 10) {
 }
 
 //using while loop without a counter[i]
-// let dice = Math.trunc(Math.random() + 6) + 1;
-// while (dice !== 6) {
-//   console.log(`you rolled a ${dice}`);
-//   dice = Math.trunc(Math.random() + 6) + 1;
-//   if (dice === 6) {
-//     console.log('Loop is about to End');
-//   }
-//   dice++;
-// }
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) {
+    console.log('Loop is about to End');
+  }
+}
